a70e5245de4300278163156ca468380d978081e0
Matthew Kantor <matthewkantor@matthews-mbp.cbc-rc.ca>
2018-08-03 09:11:13 -0400

    try with a secret key

--- config/staging.py
+++ config/staging.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+SECRET_KEY="\x1d\x02\xfbq\xb5tz\xae\x01\xbf?4\xe4\xb4\x80A\xa7\xd0\xcbRX\xd5\x00\xda"
 # CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
 # CELERY_TASK_SERIALIZER = 'json'
 # CELERY_ACCEPT_CONTENT = ['json']
